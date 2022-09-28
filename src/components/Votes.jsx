aa9424ddd1ff96acfaca6cc20b404c136db436b9
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-09-28 15:42:28 +0100

    refactored handleVote, didn't like that it rerendered the whole article

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { patchArticleById } from '../utils/api'
 import ErrorPage from './ErrorPage'
 
 const Votes = ({ singleArticle, setSingleArticle }) => {
-  const [votes, setVotes] = useState(singleArticle.votes)
   const [voteChange, setVoteChange] = useState(0)
-  const [upVote, setUpVote] = useState(false)
-  const [downVote, setDownVote] = useState(false)
   const [error, setError] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
 
-  const handleVotes = voteValue => {
-    setVoteChange(voteValue)
-    patchArticleById(singleArticle.article_id, voteValue)
-      .then(({ article }) => {
-        setSingleArticle(article)
-        setVoteChange(0)
-      })
-      .catch(err => {
-        setError(true)
-        setErrorMessage(err.message)
-      })
+  // update the vote count on the server, spread the existing article object and update the votes property, also consider optimistic rendering
+
+  const handleVote = vote => {
+    setVoteChange(currVoteChange => currVoteChange + vote)
+    patchArticleById(singleArticle.article_id, vote).catch(err => {
+      setVoteChange(currVoteChange => currVoteChange - vote)
+      setError(true)
+      setErrorMessage(err.message)
+    })
   }
 
   return (
     <>
       <div>
         <p>Votes: {singleArticle.votes + voteChange}</p>
-        <button onClick={() => handleVotes(1)}>Upvote</button>
-        <button onClick={() => handleVotes(-1)}>Downvote</button>
+        <button onClick={() => handleVote(1)}>Upvote</button>
+        <button onClick={() => handleVote(-1)}>Downvote</button>
       </div>
     </>
   )
