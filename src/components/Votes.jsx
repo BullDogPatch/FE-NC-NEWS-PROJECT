2047fa3122e31e8756dd73715acf8d406832fa03
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-09-29 10:58:03 +0100

    refactored to remove some state and use one state to also disable button

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,41 +1,38 @@
 import { useState } from 'react'
 import { patchArticleById } from '../utils/api'
 
-const Votes = ({ singleArticle, setSingleArticle }) => {
+const Votes = ({ singleArticle }) => {
   const [voteChange, setVoteChange] = useState(0)
-  const [hasVoted, setHasVoted] = useState(false)
   const [error, setError] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
 
-  const handleVote = vote => {
-    if (hasVoted) return
-    setVoteChange(currVoteChange => currVoteChange + vote)
-    setHasVoted(true)
-    patchArticleById(singleArticle.article_id, vote).catch(err => {
-      setVoteChange(currVoteChange => currVoteChange - vote)
-      setHasVoted(false)
-      setError(true)
-      setErrorMessage(err.message)
-    })
+  const handleVoteChange = vote => {
+    if (voteChange === 0) {
+      setVoteChange(curr => curr + vote)
+      patchArticleById(singleArticle.article_id, vote).catch(err => {
+        setVoteChange(curr => curr - vote)
+        setError(true)
+        setErrorMessage(err.message)
+      })
+    }
   }
 
   if (error) return <p>{errorMessage}</p>
-
   return (
     <>
       <div className="votes">
         <p>Votes: {singleArticle.votes + voteChange}</p>
         <button
-          disabled={hasVoted}
+          disabled={voteChange === 1}
           className="vote-btn"
-          onClick={() => handleVote(1)}
+          onClick={() => handleVoteChange(1)}
         >
           Upvote
         </button>
         <button
-          disabled={hasVoted}
+          disabled={voteChange === -1}
           className="vote-btn"
-          onClick={() => handleVote(-1)}
+          onClick={() => handleVoteChange(-1)}
         >
           Downvote
         </button>
