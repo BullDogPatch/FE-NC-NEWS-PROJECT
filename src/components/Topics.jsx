426b1c46dc32f77e48492404a1b2a6e948a30cb7
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-09-30 10:22:12 +0100

    refactor to make error handling for topics

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -3,20 +3,31 @@ import { useParams } from 'react-router-dom'
 import { getTopicArticles } from '../utils/api'
 import Article from './Article'
 import Loading from './Loading'
+import ErrorPage from './ErrorPage'
 
 const Topics = () => {
   const [articles, setArticles] = useState([])
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
   const { topic } = useParams()
 
   useEffect(() => {
+    setError(false)
     setLoading(true)
-    getTopicArticles(topic).then(articlesFromApi => {
-      setArticles(articlesFromApi)
-      setLoading(false)
-    })
+    getTopicArticles(topic)
+      .then(articlesFromApi => {
+        setArticles(articlesFromApi)
+        setLoading(false)
+      })
+      .catch(err => {
+        setError(true)
+        setErrorMessage(err.message)
+        setLoading(false)
+      })
   }, [topic])
 
+  if (error) return <ErrorPage errorMessage={errorMessage} />
   if (loading) return <Loading />
 
   return (
